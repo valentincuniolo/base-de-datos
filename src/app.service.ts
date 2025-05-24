b7af034003923214bc5349e4a92c8a811e5d407e
valentincuniolo <168040474+valentincuniolo@users.noreply.github.com>
2025-05-23 21:11:44 -0300

    actualizaciones

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
 
+export interface itrack {
+  id:number;
+  tittle:string;
+  duration:number;
+  artist:string;
+}
+
+export const tracks : itrack[] = [
+
+ {
+    id: 1,
+    tittle: "Imagine",
+    duration: 6,
+    artist: "John Lennon"
+  },
+
+   {
+    id: 2,
+    tittle: "loca",
+    duration: 7,
+    artist: "ariana grande"
+  },
+
+   {
+    id: 3,
+    tittle: "marginale",
+    duration: 5,
+    artist: "paul scholles"
+  }
+
+
+  
+  
+]
+
+
+
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks(): itrack[] {
+    return tracks;
   }
 }
