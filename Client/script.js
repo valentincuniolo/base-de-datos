12abe29684eac468cc31cbe406d89d6530ec30a8
valentincuniolo <168040474+valentincuniolo@users.noreply.github.com>
2025-06-09 21:48:50 -0300

    wd

--- Client/script.js
+++ Client/script.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener('DOMContentLoaded"',()=>{
     const obtenerDatos=document.getElementById("obtener datos")
-    obtenerDatos.addEventListener("click",()=>{
+    obtenerDatos.addEventListener('click',()=>{
 
-fetch("http://localhost:3000/track")
+fetch('http://localhost:3000/track')
 .then(res=>res.json())
 .then(data=>{
-    const contenedor=document.querySelector("#lista-canciones");
-    contenedor.innerHTML="";
+    const contenedor=document.querySelector('#lista-canciones');
+    contenedor.innerHTML='';
     data.forEach(cancion => {
 
-        const itemCancion= document.createElement("li");
-        const detalles= document.createElement("ul");
+        const itemCancion= document.createElement('li');
+        const detalles= document.createElement('ul');
 
 
 Object.entries(cancion).forEach(([clave,valor])=>{
 
-    const lidetalle=document.createElement("li")
+    const lidetalle=document.createElement('li')
     lidetalle.textContent= `${clave}: ${valor}`;
     detalles.appendChild(lidetalle)
 
