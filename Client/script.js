48b72204930ae568f5f9c29440729c383dc9e94b
valentincuniolo <168040474+valentincuniolo@users.noreply.github.com>
2025-06-09 21:43:54 -0300

    mi

--- Client/script.js
+++ Client/script.js
@@ -2,7 +2,35 @@ document.addEventListener("DOMContentLoaded",()=>{
     const obtenerDatos=document.getElementById("obtener datos")
     obtenerDatos.addEventListener("click",()=>{
 
-fetch()
+fetch("http://localhost:3000/track")
+.then(res=>res.json())
+.then(data=>{
+    const contenedor=document.querySelector("#lista-canciones");
+    contenedor.innerHTML="";
+    data.forEach(cancion => {
+
+        const itemCancion= document.createElement("li");
+        const detalles= document.createElement("ul");
+
+
+Object.entries(cancion).forEach(([clave,valor])=>{
+
+    const lidetalle=document.createElement("li")
+    lidetalle.textContent= `${clave}: ${valor}`;
+    detalles.appendChild(lidetalle)
+
+
+
+});
+
+itemCancion.appendChild(detalles)
+contenedor.appendChild(itemCancion)
+
+
+
+
+    });
+})
 
     })
 
