7393ae378cf6ba8471318c244f1cc1c6fbb10f8c
valentincuniolo <168040474+valentincuniolo@users.noreply.github.com>
2025-06-09 21:17:49 -0300

    actu

---
+++ Client/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const obtenerDatos=document.getElementById("obtener datos")
+    obtenerDatos.addEventListener("click",()=>{
+
+fetch()
+
+    })
+
+
+
+})
